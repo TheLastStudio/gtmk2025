639d077e0d6162f27461aff8519322a42aa1b0c8
QuaZi42 <andrii.medmid@gmail.com>
2025-07-31 00:20:34 -0400

    Day 1 End

--- entities/car/car.gd
+++ entities/car/car.gd
@@ -5,16 +5,24 @@ enum {SETTING, ORBITING}
 var state = SETTING
 var game : Game
 
+var base_max_launch_speed = 600
+var max_launch_speed = 600
+var launch_dir
+
+
 func _ready() -> void:
 	game = get_tree().get_first_node_in_group("game")
 	randomize()
 	modulate = Color(randf(), randf(), randf())
 
 func _physics_process(_delta: float) -> void:
+	max_launch_speed = base_max_launch_speed*sqrt(1/(get_parent().station.position - get_parent().planet.position).length())*11.7
+	launch_dir = get_global_mouse_position() - get_parent().station.position
 	if state == SETTING:
+		position = get_parent().station.position
 		if dragging:
-			$Line2D.points[1] = -drag_start + get_global_mouse_position()
-			generate_path((-get_global_mouse_position() + drag_start)*2)
+			$Line2D.points[1] = (launch_dir.normalized())*min(max_launch_speed, launch_dir.length())
+			generate_path(-(launch_dir.normalized())*2*min(max_launch_speed, launch_dir.length()))
 	elif state == ORBITING:
 		var mouse_pos = get_global_mouse_position()
 		mouse_pos = game.planet.global_position
@@ -36,27 +44,30 @@ func _input(event: InputEvent) -> void:
 				elif dragging == true:
 					dragging = false
 					state = ORBITING
-					linear_velocity = (-get_global_mouse_position() + drag_start)*2
+					linear_velocity = -(launch_dir.normalized())*2*min(max_launch_speed, launch_dir.length())
 					$Line2D.hide()
 					game.station.add_new()
 					collision_layer = 0b00000001
 					collision_mask = 0b00000001
+					generate_path(-1)
 					#linear_velocity = Vector2.RIGHT*(sqrt(.2e10/mass/(global_position - $"../Center".global_position).length()))*1.25
-
 func generate_path(velocity):
-	var GMm = .2e10
-	var delta = 1.0/60
-	var pos = Vector2.ZERO
-	var center_position = game.planet.global_position - global_position
-	var accel = Vector2.ZERO
-	game.station.path.clear_points()
-	game.station.path.modulate = Color.WHITE
-	
-	for x in 7000:
-		game.station.path.add_point(pos)
-		accel = ((center_position - pos).normalized() * GMm/((center_position - pos).length()**2))/mass
-		velocity += accel*delta
-		pos += velocity*delta
-		if (center_position - pos).length() <= 150:
-			game.station.path.modulate = Color.RED
-			break
+	if velocity is not int:
+		var GMm = .2e10
+		var delta = 1.0/60
+		var pos = Vector2.ZERO
+		var center_position = game.planet.global_position - global_position
+		var accel = Vector2.ZERO
+		game.station.path.clear_points()
+		game.station.path.modulate = Color.WHITE
+		
+		for x in 7000:
+			game.station.path.add_point(pos)
+			accel = ((center_position - pos).normalized() * GMm/((center_position - pos).length()**2))/mass
+			velocity += accel*delta
+			pos += velocity*delta
+			if (center_position - pos).length() <= 95:
+				game.station.path.modulate = Color.RED
+				break
+	else:
+		game.station.path.clear_points()
--- entities/station/station.gd
+++ entities/station/station.gd
@@ -3,6 +3,8 @@ extends CharacterBody2D
 @onready var path = $Path
 @onready var car = preload("res://entities/car/car.tscn")
 
+var speed = 200
+
 func _ready() -> void:
 	add_new()
 
@@ -10,3 +12,12 @@ func add_new():
 	var c = car.instantiate()
 	c.global_position = global_position
 	get_tree().get_first_node_in_group("game").add_child.call_deferred(c)
+
+func _physics_process(delta: float) -> void:
+	process_input()
+	
+	move_and_slide()
+
+func process_input():
+	var input_direction = Input.get_action_strength("station_right") - Input.get_action_strength("station_left")
+	velocity = Vector2(input_direction * speed, 0)
